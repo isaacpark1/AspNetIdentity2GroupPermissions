c0b67f99062f0571c2ee96179f70e46cadd79550
isaacpark1 <isaacpark@yahoo.com>
2015-05-26 12:28:18 -0400

    initial check in

---
+++ BMISP/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using BMISP.Models;
+using System.Data.Entity;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BMISP
+{
+    // Note: For instructions on enabling IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=301868
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ BMISP/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace BMISP.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BMISP.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "BMISP.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(BMISP.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
